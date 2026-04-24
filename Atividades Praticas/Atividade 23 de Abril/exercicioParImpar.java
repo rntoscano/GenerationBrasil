2e7307746e1dd8ff0dcec939dad57ebb81d64c67
rntoscano <anny.toscano@hotmail.com.com>
2026-04-23 23:55:51 -0300

    novas atividades

--- Atividades Praticas/Atividade 23 de Abril/exercicioParImpar.java
+++ Atividades Praticas/Atividade 23 de Abril/exercicioParImpar.java
@@ -4,24 +4,30 @@ import java.util.Scanner;
 
 public class exercicioParImpar {
 
-	public static void main(String[] args) {
-		 Scanner leia = new Scanner(System.in);
-		 
-		int numero;
-		
-		
-		for (numero = 1; numero < 5; numero ++);
-		
-		System.out.println("Digite um numero: ");
-		numero = leia.nextInt();
-		
-		if(numero % 2 == 0) {
-			System.out.println("Número é Par ");
-		
-		}else {
-			System.out.println("Número é Impar");
-		
-	}
+	 public static void main(String[] args) {
 
-}
-}
+	        Scanner leia = new Scanner(System.in);
+
+	        int numero;
+	        int par = 0;
+	        int impar = 0;
+
+	        for (int i = 1; i <= 10; i++) {
+
+	            System.out.print("Digite o " + i + "º número: ");
+	            numero = leia.nextInt();
+
+	            if (numero % 2 == 0) {
+	                par++;
+	            } else {
+	                impar++;
+	            }
+	        }
+
+	        System.out.println("\nTotal de números pares: " + par);
+	        System.out.println("Total de números ímpares: " + impar);
+
+	     
+	    }
+	}
+	
