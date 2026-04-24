6dcd2cf070e515cd25c9ac99963dd0fce727b548
rntoscano <anny.toscano@hotmail.com.com>
2026-04-23 23:23:51 -0300

    novas atividades

---
+++ Atividades Praticas/Atividade 23 de Abril/exercicioParImpar.java
@@ -0,0 +1,27 @@
+package terceiraAtividade;
+
+import java.util.Scanner;
+
+public class exercicioParImpar {
+
+	public static void main(String[] args) {
+		 Scanner leia = new Scanner(System.in);
+		 
+		int numero;
+		
+		
+		for (numero = 1; numero < 5; numero ++);
+		
+		System.out.println("Digite um numero: ");
+		numero = leia.nextInt();
+		
+		if(numero % 2 == 0) {
+			System.out.println("Número é Par ");
+		
+		}else {
+			System.out.println("Número é Impar");
+		
+	}
+
+}
+}
